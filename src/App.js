70bca5dfb816be86aba9c746e37eb18929bb05e0
Aadithya2804 <113664862+Aadithya2804@users.noreply.github.com>
2023-07-11 12:12:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import React from 'react';
+import Form from './components/Form';
+import Login from './components/login';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+;
+      
+function App() {
+  return (
+    // <BrowserRouter>
+    <Navbar />
+  );
+}
+  
+export default App;
